d3b16adaf3f3185ea7da2a965309e847845ae1dd
Dalton <dalton.campos20@gmail.com>
2022-12-21 19:37:30 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/pages/HomePage";
+import SearchPage from "./src/pages/SearchPage";
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Search Page" component={SearchPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./style";
+
+function SearchPage() {
+  return (
+    <View style={styles.container}>
+      <Text>Essa é a Search Page</Text>
+    </View>
+  );
+}
+
+export default SearchPage;
